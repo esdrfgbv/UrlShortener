090f133ad362ef15ae23e9236d4534c673b0ba0e
esdrfgbv <subrahmanyamkolipakula@gmail.com>
2026-03-04 19:36:23 +0530

    added generation strategy

--- src/main/java/com/example/url/model/Url.java
+++ src/main/java/com/example/url/model/Url.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 
@@ -11,7 +12,7 @@ import javax.persistence.Lob;
 public class Url {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Lob
